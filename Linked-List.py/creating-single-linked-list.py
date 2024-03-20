4e320821bbffd47a62c8630a329651581d644c1c
ujjwal-basnet <ub.lovetocode@gmail.com>
2024-03-20 16:31:30 +0545

    creatre single linkenlist

---
+++ Linked-List.py/creating-single-linked-list.py
@@ -0,0 +1,23 @@
+#check-out my medium for explanation 
+
+class Node():
+    def __init__(self , value = None ):
+        self.value = value 
+        self.next = None 
+        
+    
+class SLinkedList():
+    def __init__(self):
+        self.head = None 
+        self.tail = None
+        
+        
+        
+    singlyLinkednlist  =SLinkedList()
+    node1 = Node(1)
+    node2 = Node(2)
+    
+    
+    singlyLinkednlist.head = node1 
+    singlyLinkednlist.head.next =  node2 
+    singlyLinkednlist.tail = node2 
