c23724832187e60167341ac7bfeb0f3dab51ac61
ujjwal-basnet <ub.lovetocode@gmail.com>
2024-03-21 17:34:50 +0545

    create node and singlelinked lsit

---
+++ Linked-List/insertion-single-linked-list.py
@@ -0,0 +1,33 @@
+class Node :
+    def init__(self  , value = None ):
+        self.value  = value 
+        self.next = next 
+        
+class SlinkedList:
+    def __init__(self):
+        self.head = None 
+        self.tail = None 
+        
+        def __iter__(self):
+            node = self.head 
+            while node :
+                yield node 
+                node = node.next 
+                
+                #insert in linked list 
+        
+        #create a method which takes two parameter   , value and location
+        def insertSLL(self , value , location ):
+            newNode = Node(value)
+            newNode = Node(value)
+            
+            
+            
+            
+            
+            
+                
+        
+                
+                
+                
