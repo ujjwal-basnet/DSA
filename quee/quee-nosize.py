246e8669a98524bd4c4483625b82d4dae349f238
ujjwal-basnet <ub.lovetocode@gmail.com>
2024-03-24 18:29:17 +0545

    queue no size specified done by list

--- quee/quee-nosize.py
+++ quee/quee-nosize.py
@@ -14,13 +14,43 @@ class Queue:
         else :
             return False 
         
+    
+    def enqueue(self , value):
+        self.items.append(value)
+        return "The elements is inserted at the end of the queee"
+        
+        
+    def dequeue(self):
+        if self.isEmpty():
+            return "The elements is inserted at the end of the Queue"
+        else :
+            return self.items.pop(0)
+        
+    def peek(self):
+        if self.isEmpty():
+            return "No Elements on Quee" 
+        else :
+            return self.items[0]
+        
+        
+    def delete(self):
+        if self.isEmpty():
+            return "No Elements to deelete"
+        else :
+            self.items = None 
+            return self.items
 
 
 sampleQueue = Queue()
-Queue.push(1)
-Queue.push(3)
-Queue.push(4)
-Queue.push(6)
+sampleQueue.enqueue(1)
+sampleQueue.enqueue(2)
+sampleQueue.enqueue(3)
+sampleQueue.enqueue(4)
+sampleQueue.enqueue(5)
+print(sampleQueue)
+print(sampleQueue.dequeue())
+print(sampleQueue.peek())
+print(sampleQueue.delete())
 
 
         
