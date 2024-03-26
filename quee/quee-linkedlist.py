1b456dbee3c3718214b83920763fe2091087a96b
ujjwal-basnet <ub.lovetocode@gmail.com>
2024-03-26 12:41:26 +0545

    Simple git with linked list

---
+++ quee/quee-linkedlist.py
@@ -0,0 +1,61 @@
+class Node:
+    def __init__(self , value):
+        self.value = value 
+        self.next = None 
+        
+        
+    def __str__(self):
+        return str(self.value)
+        
+
+class Linkedlist:
+    
+    def __init__(self):
+        self.head = None  
+        self.tail = None 
+        
+    def __iter__(self):
+        node = self.head
+        while node :
+            yield node 
+            node = node.next 
+        
+        
+
+class Queue:
+    def __init__(self):
+        self.linkedList = Linkedlist()
+        
+    
+    def __str__(self):
+        values = [str(x) for x in self.linkedList]
+        return ' , '.join(values)
+    
+    #inserting value code 
+    def enqueue(self , value ):
+        newNode = Node(value)
+        
+        
+        if self.linkedList.head ==  None :
+            self.linkedList.head = newNode 
+            self.linkedList.tail = newNode 
+        
+        else :
+            self.linkedList.tail.next = newNode 
+            self.linkedList.tail= newNode 
+        
+
+        
+customQUeue = Queue()
+print(customQUeue.enqueue(3))
+print(customQUeue.enqueue(4))
+print(customQUeue.enqueue(5))
+print(customQUeue)
+            
+        
+
+print(customQUeue)
+        
+        
+        
+        
