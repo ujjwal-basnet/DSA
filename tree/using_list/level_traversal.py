b82a77f88c1e310b978cc61e11c61727ab81874f
Ujjwal Basnet <ujjwalvictus15@gmail.com>
2024-04-21 20:54:04 +0545

    level travesal in list

---
+++ tree/using_list/level_traversal.py
@@ -0,0 +1,28 @@
+class BinaryTree():
+    def __init__(self , size):
+        self.CustomList = size * [None]
+        self.LastUsedIndex = 0 
+        self.MaxSize = size 
+
+
+    def insertNode(self , value ):
+        if self.LastUsedIndex +1 == self.MaxSize :
+            return "Tree is Full "
+        
+        else :
+            self.CustomList[self.LastUsedIndex + 1 ]= value
+            self.LastUsedIndex += 1 
+            return "Insertition Sucessfully"
+        
+
+    def Level_travesel(self):
+        for i in range(self.LastUsedIndex + 1 ):
+            print(self.CustomList[i])
+        
+
+tree  = BinaryTree(5)
+tree.insertNode("A")
+tree.insertNode("B")
+tree.insertNode("C")
+tree.insertNode("D")
+tree.Level_travesel()
