33dbaa1d429f5184f415110cc80baa11f7259d1a
Ujjwal Basnet <ujjwalvictus15@gmail.com>
2024-04-21 22:01:54 +0545

    BinarySearchTree added

---
+++ BinarySearch_Tree/Using_LinkedLIst/simpleBst.py
@@ -0,0 +1,16 @@
+
+import collections 
+
+
+class Bst():
+    def __init__(self , data ):
+        self.data = data 
+        self.LeftChild = None 
+        self.RightChild = None 
+
+
+newBst = Bst("fgasd")
+print(newBst.data)
+print(newBst.LeftChild)
+print(newBst.RightChild)
+
